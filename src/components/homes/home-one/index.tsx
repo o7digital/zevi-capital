2f7d0de66c9605aded2b4c827cb5228db9872492
o7digital <101292703+o7digital@users.noreply.github.com>
2026-05-23 02:40:22 -0600

    Remove about section from home page

--- src/components/homes/home-one/index.tsx
+++ src/components/homes/home-one/index.tsx
@@ -3,7 +3,6 @@ import HeaderOne from "@/layouts/headers/HeaderOne"
 import Banner from "./Banner"
 import Feedback from "./Feedback"
 import BLockFeatureOne from "./BLockFeatureOne"
-import BLockFeatureTwo from "./BLockFeatureTwo"
 import BLockFeatureThree from "./BLockFeatureThree"
 import Property from "./Property"
 import BLockFeatureFive from "./BLockFeatureFive"
@@ -16,7 +15,6 @@ const HomeOne = () => {
       <HeaderOne style={false} />
       <Banner />
       <BLockFeatureOne />
-      <BLockFeatureTwo />
       <BLockFeatureThree />
       <Property />
       {/* <AgentArea style={false} /> */}
